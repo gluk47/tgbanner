845a01d35af97b2e81ea626255764300c7f0655b
Egor Minix <gluk47@gmail.com>
2025-05-04 21:03:08 +0200

    pep8

--- text.py
+++ text.py
@@ -1,16 +1,20 @@
 import re
 
+
 class Normalizer:
     def __init__(self):
-        self.discard = re.compile(pattern = '['
-            u'\U0001F600-\U0001F64F'  # emoticons
-            u'\U0001F300-\U0001F5FF'  # symbols & pictographs
-            u'\U0001F680-\U0001F6FF'  # transport & map symbols
-            u'\U0001F1E0-\U0001F1FF'  # flags (iOS)
-            u'\U00002702-\U000027B0'
-            u'\U000024C2-\U0001F251'
-            u'\U0001f926-\U0001f937'
-        ']+', flags = re.UNICODE)
+        self.discard = re.compile(
+            pattern='['
+                    u'\U0001F600-\U0001F64F'  # emoticons
+                    u'\U0001F300-\U0001F5FF'  # symbols & pictographs
+                    u'\U0001F680-\U0001F6FF'  # transport & map symbols
+                    u'\U0001F1E0-\U0001F1FF'  # flags (iOS)
+                    u'\U00002702-\U000027B0'
+                    u'\U000024C2-\U0001F251'
+                    u'\U0001f926-\U0001f937'
+                    ']+',
+            flags=re.UNICODE
+        )
 
         self.upper_table = str.maketrans(
             'BHN',
@@ -47,11 +51,10 @@ class Normalizer:
         else:
             return ' '.join([text_orig, text])
 
+
 _NORMALIZER = Normalizer()
 
 
 def normalize(txt: str) -> str:
     ret = _NORMALIZER.apply(txt)
-    print(f'«{txt}» -> «{ret}»')
     return ret
-
